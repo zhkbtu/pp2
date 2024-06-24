40ac05e1869f488ae8ad9c433a3d4cc8aedd5580
zhkbtu <dauenzadyra@gmail.com>
2024-06-24 10:32:19 +0500

    Delete pp2_summer/5.py

--- pp2_summer/5.py
+++
@@ -1,8 +0,0 @@
-#Write a function that accepts string from user and print all permutations of that string
-from itertools import permutations
-def per(s):
-    p = [''.join(p) for p in permutations(s)]
-    return p
-s = str(input())
-
-print(per(s))
