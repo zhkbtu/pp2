f1cca7cc5a11478002fbbdae0b4f82cce3e8118e
zhkbtu <dauenzadyra@gmail.com>
2024-06-24 10:31:52 +0500

    Delete pp2_summer/2.py

--- pp2_summer/2.py
+++
@@ -1,6 +0,0 @@
-#Read in a Fahrenheit temperature. Calculate and display the equivalent centigrade temperature. The following formula is used for the conversion: C = (5 / 9) * (F – 32)
-x=int(input())
-def f_to_c(x):
-    return (5.0/9.0) * (x - 32)
-res=f_to_c(x)
-print ("{0} fahrenheit is {1} centigrade".format(x, res))
